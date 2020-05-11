1793310692113eec0aab5b4843ea6afecbc5c711
ivanfslee <ivanfslee@gmail.com>
2020-05-11 00:09:56 -0700

    Added notes about MongoDB vs SQL and the MongoDB ecosystem

--- overview.js
+++ overview.js
@@ -6,8 +6,7 @@
         //Collections - 'Users' , 'Orders'
             //Documents
     
-    //Non-relational database which leads to flexibility
-        //Schemaless
+    //Non-relational database (schemaless) which leads to flexibility
 
 // Documents have a JSON (BSON) Data Format
     // {
@@ -42,4 +41,53 @@
 //The main takeaway for mongodb is its flexibility
     //and its optimization for usability
     //You can query data in the format you need,
-    //rather than running complex restructurings on the server. 
+    //rather than running complex restructurings on the server.
+
+//MongoDV is a NoSQL solution
+    //Which differs from that of SQL based
+
+    //Instead of normalizing data - that is, storing data distributed
+        //across multiple tables, where every table
+        //has a clear schema and then using lots of relations,
+    
+    //MongoDB goes for storing data together in a document
+        //and it doesn't force a schema on you
+    
+    //Documents in a collection in MongoDB can have different structures
+        //Caveat: This can lead to messy data
+        //Upside: This gives you a lot of flexibility
+    
+    //You work with No/Fewer Relations
+        //Since data is stored together, when your application
+        //is fetching data, it doesn't have to reach out to 
+        //collection 'A', merge it with collection 'B', merge with others
+    
+    //Instead, MongoDB can go to collection 'A', 
+        //then MongoDB has a very efficient querying mechanism
+        //and goes through the collection 'A' quickly to look for 
+        //a specific document
+        //Most of the time, MongoDB doesn't have to do alot of merging
+    
+    //No schema, non-relational data leads to MongoDB's speed, performance, and flexibility
+
+//MongoDB ecosystem
+    //MongoDB Database
+
+    //Tools to manage the database
+
+    //Atlas (Cloud) -
+
+    //Mobile
+
+    //Compass - GUI to manage database
+
+    //BI Connectors
+
+    //MongoDB Charts
+
+    //Stitch - Serverless backend solution
+        //Offers more than just data storage
+        //Serverless Query API
+        //Serverless functions - allow you to execute code in the cloud on demand
+    
+    //In this course, we will use the MongoDB locally and in the cloud
