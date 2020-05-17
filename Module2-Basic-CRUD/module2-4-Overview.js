87a139babb673f375168bb26cb75f6408c6271ec
ivanfslee <ivanfslee@gmail.com>
2020-05-17 00:19:04 -0700

    Added code and notes on how to access arrays in documents and embedded documents

--- Module2-Basic-CRUD/module2-4-Overview.js
+++ Module2-Basic-CRUD/module2-4-Overview.js
@@ -87,4 +87,189 @@ Embedded Documents
     Another kind of data you can store are arrays.
         You can have arrays of embedded documents.
         But arrays can hold any data.
+
+        e.g. {hobbies: ['sports', 'cooking','painting']} - document that has an array of strings
+        e.g. [{...}, {...}, {...}] - array of embedded documents
+        e.g. [1,23,4,5,6] - array of numbers
+
+
+Working with Nested/Embedded Documents
+
+    Here, we add another property called 'status' 
+        that contains an embedded document.
+        The embedded doc has 2 properties, 
+        'description' and 'lastUpdated'
+
+        db.flightData.updateMany({}, {$set: 
+            {
+                status: { 
+                    description: "on-tine",
+                    lastUpdated: "1 hour ago"
+                }
+            }
+        })
+    
+    
+    The resulting document will look like so:
+
+        {   
+            "_id": ObjectId("sldjfsdlfj"),
+            "departureAirport": "MUC",
+            "arrivalAirport": "SFO",
+            "aircraft": "Airbus A380",
+            "distance": 12000,
+            "intercontinental": true
+            "status": { 
+                "description": "on-tine",
+                "lastUpdated": "1 hour ago"
+            }          
+        }
+    
+    We can add another embedded document doing the same thing:
+
+        db.flightData.updateMany({}, {$set: 
+            {
+                status: { 
+                    description: "on-tine",
+                    lastUpdated: "1 hour ago",
+                    details: {
+                        responsible: "Ivan Lee"
+                    }
+                }
+            }
+        })
+    
+    That results in:
+
+        {   
+            "_id": ObjectId("sldjfsdlfj"),
+            "departureAirport": "MUC",
+            "arrivalAirport": "SFO",
+            "aircraft": "Airbus A380",
+            "distance": 12000,
+            "intercontinental": true
+            "status": { 
+                "description": "on-tine",
+                "lastUpdated": "1 hour ago",
+                "details": {
+                    "responsible": "Ivan Lee"
+                }
+            }          
+        }
+
+Working with arrays - Storing Arrays of data
+    
+        We will work with our passengers collection for this example.
+
+    A portion of our passengers collection looks like so:
+
+        }
+
+            "_id" : ObjectId("5ec058f4e1ec51bd5e94ddbb"),
+            "name" : "Armin Glutch",
+            "age" : 35
+        }
+
+            "_id" : ObjectId("5ec058f4e1ec51bd5e94ddbc"),
+            "name" : "Klaus Arber",
+            "age" : 53
+        }
+
+            "_id" : ObjectId("5ec058f4e1ec51bd5e94ddbd"),
+            "name" : "Albert Twostone",
+            "age" : 68
+        }
+    
+    We add a 'hobbies' property to one of the documents:
+
+    db.passengers.updateOne({name: "Albert Twostone"}, {$set: 
+        {
+            hobbies: [
+                'sports',
+                'cooking',
+                {someObject: 'someString'},
+                3
+            ]          
+        }
+    })
+
+
+    The resulting document looks like this:
+
+    {
+        "_id" : ObjectId("5ec058f4e1ec51bd5e94ddbd"),              
+        "name" : "Albert Twostone",                                
+        "age" : 68,                                                
+        "hobbies" : [                                              
+                "sports",                                          
+                "cooking",                                         
+                {                                                  
+                  "someObject" : "someString"                
+                },                                                 
+                3                                                  
+        ]                                                          
+    }
+
+    As we can see then, we can store arrays inside a document.
+        And inside those arrays, you can store 
+        strings, numbers, and even other documents
+
+Accessing Structured Data and arrays
+
+    Let's access the 'hobbies' array we just added.
+    Note: We need to use 'findOne()' because that will give us access to 
+        the doc's properties.
+        If we use 'find()', it will not work
+
+        db.passengers.findOne({name: "Albert Twostone"}).hobbies
+
+        This would return the 'hobbies' array:
+
+            ["sports", "cooking", {"someObject": "someString"}, 3]
+    
+    If we want to find all passengers with the 'hobby' of 'sports'
+
+        db.passengers.find({hobbies: "sports"}).pretty()
+    
+    So, from the last example mongo can go through the 
+        'hobbies' array and look for "sports" value.
+    
+
+Accessing embedded documents
+
+    Given our flights data document that has an embedded doc:
+
+        {   
+            "_id": ObjectId("sldjfsdlfj"),
+            "departureAirport": "MUC",
+            "arrivalAirport": "SFO",
+            "aircraft": "Airbus A380",
+            "distance": 12000,
+            "intercontinental": true
+            "status": { 
+                "description": "on-tine",
+                "lastUpdated": "1 hour ago",
+                "details": {
+                    "responsible": "Ivan Lee"
+                }
+            }          
+        }  
+    
+
+    If we want to see which documents have 'description' property
+        that has a value of 'on-time', we use this query:
+
+            db.flightData.find({"status.description": "on-time"}).pretty()
+
+        We use dot notation to access embedded documents and that
+            is wrapped around double quotation marks, so we get:
+            
+                "status.description"
+    
+    To go further into our embedded doc:
+
+            db.flightData.find({"status.details.responsible": "Ivan Lee"}).pretty()
+
+        This query will see which doc has 'responsible' set to 'Ivan Lee'
+
 */
