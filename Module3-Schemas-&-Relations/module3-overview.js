1a55cb48115cabcba5f3721e66114912e8bc440f
ivanfslee <ivanfslee@gmail.com>
2020-05-18 00:38:57 -0700

    Added ntoes and code for Schema vs Schema-less and the use of 'null'

--- Module3-Schemas-&-Relations/module3-overview.js
+++ Module3-Schemas-&-Relations/module3-overview.js
@@ -16,6 +16,10 @@ Data Schemas & Data Modelling
 
         db.<collection name>.drop()
 
+    To delete all documents in a collection:
+
+        db.<collection name>.deleteMany({})
+
 In this module we will go over the following:
 
     Decide how we model our data
@@ -42,4 +46,33 @@ Why do we use Schemas?
         and consistent across your data.
 
 Structuring Documents
+
+    No Schema ------------------------------------- SQL 
+
+    Imagine a continuum - one extreme is no schema at all
+        The other extreme is that all documents have the same schema.
+    
+    In the middle is, generally, documents have the same schema
+        but some documents have extra information and extra
+        properties that other documents don't have.
+    
+    Typically, you will err on the side of the middle or towards
+        the right side of the continuum. 
+    
+    In the SQL world, if you don't have data for a certain property
+        you will have a null value.
+    In the middle, you will simply not have that property at all.
+
+    In the SQL approach, you may use it if you like the clarity and
+        consistency of the same fields in each document.
+    
+    There is no convention here. Use whichever approach you prefer.
+    
+Data Type
+
+    'double' number type is a number with decimals - e.g. 12.99
+    'integer' number type is a number without decimals - e.g. 12
+
+    'null' data type - value you can assign to indicate there is 
+        no value but there is a field there.
 */
