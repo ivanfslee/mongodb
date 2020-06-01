af11aa9d7095808ed04092485487fd27b121a896
ivanfslee <ivanfslee@gmail.com>
2020-06-01 16:26:10 -0700

    Added notes on relation vs embedded data relations.

--- Module3-Schemas-&-Relations/module3-13-summarizing-relations.js
+++ Module3-Schemas-&-Relations/module3-13-summarizing-relations.js
@@ -1,6 +1,16 @@
 /*
 Data Relations Summary 
 
+    Your approach always depends on your use-case
+        and application needs.
+        - how do you use your data
+        - how often do I use it
+        - how often you change data
+        - how is the data related - one-to-one, one-to-many, many-to-many
+        - if snapshot data does suffice
+        - if you need the most current data available always
+        - how much data you have
+
     You have 2 options:
         1. Nested/Embedded Documents
         2. References
@@ -8,10 +18,15 @@ Data Relations Summary
 Nested/Embedded Documents
 
     Group data together logically
+        -Great for data that belongs together
+        -one-to-many or one-to-one relations
 
     Great for data that belongs together and is
         not really overlapping with other data
-    
+        - no many-to-many relations
+        - less than 16mb and 100 nesting limits
+        - You don't need to split your data for application needs
+
     Avoid super-deep nesting (100+ levels) or
         extremely long arrays 
         (16mb size limit per document)
@@ -23,8 +38,16 @@ References
     Great for related but shared data as well as
         for data which is used in relations and
         standalone
-    
+        -good for many-to-many relations
+        -good for data where you would have alot of 
+            duplicates that you need to update alot
+            (which you want to avoid, so you use references)
+        
     Allows you to overcome nesting and size limits
         (by creating new documents)
+        - if you run into the issue of documents
+            reaching 16mb or 100+ nesting, then 
+            you should use references to split
+            the data across multiple documents/collections
 
 */
