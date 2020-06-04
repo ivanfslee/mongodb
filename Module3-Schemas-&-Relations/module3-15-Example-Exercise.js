60cb8165e6e3d7a12e6ecbb634e6f3269c636b54
ivanfslee <ivanfslee@gmail.com>
2020-06-04 00:08:38 -0700

    Added notes for example blog problem. How would you structure the data in your mongodb?

---
+++ Module3-Schemas-&-Relations/module3-15-Example-Exercise.js
@@ -0,0 +1,37 @@
+/*
+Example Project: A Blog
+
+
+    Think about how you would structure the data.
+
+User - in our application (server side rendered website/ single page app / mobile app/ etc)
+
+    Application -> User -> App Server -> MongoDB Server & Database
+
+To the app server, we send a couple different requests
+
+    create posts
+    edit posts
+    delete posts
+    fetch all posts 
+    fetch single post
+    comment 
+
+App Server
+    PHP
+    .NET
+    Node.js
+
+    PHP/.NET/Node.js will use MongoDB Driver
+        to make requests to the MongoDB Server and Database
+
+Identify a schema 
+Identify the entities we have and how the 
+    schemas for these entities could look like
+    how they are related and how we would 
+    model these relations.
+
+Layout the core data entities you have and how they 
+    might look like in your application and how they
+    could be related.
+*/
