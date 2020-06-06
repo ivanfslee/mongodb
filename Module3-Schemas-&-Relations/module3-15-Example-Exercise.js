1854a50890ad42c8cd7fa9cdd4b548e05799ae69
ivanfslee <ivanfslee@gmail.com>
2020-06-06 15:36:02 -0700

    Added my schema for a blog database.

--- Module3-Schemas-&-Relations/module3-15-Example-Exercise.js
+++ Module3-Schemas-&-Relations/module3-15-Example-Exercise.js
@@ -34,4 +34,67 @@ Identify the entities we have and how the
 Layout the core data entities you have and how they 
     might look like in your application and how they
     could be related.
+
+
+
+My Data Schema for a Blog
+
+    You essentially have a one-to-many relationship.
+
+    One user will have many posts.
+    One post belongs to one user.
+
+    The question is, do I make it an 
+        embedded document or use references?
+
+        An embedded document would look like this:
+
+        We would only have one collection - 'Users'
+            that would contain an array of documents
+            where each document is a blog post
+
+
+        Embedded Blog Schema
+        
+            Users Collection
+
+                {
+                    userId: '23l4j23l423j',
+                    name: 'John Doe',
+                    posts: [
+                        {
+                            postId: 'post1',
+                            title: 'hello world',
+                            body: 'aloha world'
+                        },
+                        {
+                            postId: 'post2',
+                            title: 'vaccay',
+                            body: 'going on vacation'
+                        }
+                    ]
+                }
+
+        References Blog Schema
+
+            Users Collection 
+
+                {
+                    userId: '23423l4j23kl4j23',
+                    name: 'John Doe',
+                    posts: ['post1', 'post2']
+                }
+
+            Posts Collection
+
+                {
+                    postId: 'post1',
+                    title: 'hello world',
+                    body: 'aloha world'
+                },
+                {
+                    postId: 'post2',
+                    title: 'vaccay',
+                    body: 'going on vacation'
+                }
 */
